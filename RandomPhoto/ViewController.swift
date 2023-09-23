973c1c8bc2ddb2ffdae8947febe68ab183fd90f0
muhammadtalha <muhammed.talha@consoliads.com>
2023-09-24 03:42:50 +0500

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Muhammad Talha on 24/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
